f65b9c06112d2758b8d21679e4f71d7be665e5a5
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-05-04 10:38:12 +0100

    minor changes

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bash
 
-docker pull alperyilmaz/asciidoctor
-docker run -v ${PWD}:/documents/ alperyilmaz/asciidoctor asciidoctor-pdf -r asciidoctor-bibliography -a bibliography-database=manuscript.bibtex -d article -D /documents/manuscript-draft manuscript.adoc
+#docker pull alperyilmaz/asciidoctor
+#docker run -v ${PWD}:/documents/ alperyilmaz/asciidoctor asciidoctor-pdf -r asciidoctor-bibliography -a bibliography-database=manuscript.bibtex -d article -D /documents/manuscript-draft manuscript.adoc
+
+
+docker build -t "paperlatex" .
+docker run --name pdflatex --rm -i --user="$(id -u):$(id -g)" --net=none -v $PWD:/data paperlatex pdflatex -interaction=nonstopmode -output-dir=/data manuscript-latex.tex
